e6852e9b374b1ecd7baf1476e83fc352c9ee6dc4
leowang <rfv7855659@gmail.com>
2019-06-05 01:23:06 +0800

    完成mysql sequelize 串接

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var handlebars = require('express-handlebars');
+var db = require('./models');
 var app = express();
 
-app.engine('handlebars',handlebars());
-app.set('view engine',handlebars)
+app.engine('handlebars',handlebars({defaultLayout: 'main'}));
+app.set('view engine','handlebars')
 
 
 app.listen(3000,()=>{
+    db.sequelize.sync();
     console.log('hello');
 })
+
+require('./routes')(app);
