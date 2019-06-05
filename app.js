c76db1cfacffd0bd60c78fe06807855bd130745e
leowang <rfv7855659@gmail.com>
2019-06-05 23:47:28 +0800

    完善註冊驗證及顯示錯誤

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ var express = require('express');
 var handlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var db = require('./models');
+const flash = require('connect-flash')
+const session = require('express-session')
 var app = express();
 
 app.engine('handlebars',handlebars({defaultLayout: 'main'}));
 app.set('view engine','handlebars')
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(flash());
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 
+app.use((req,res,next)=>{
+    res.locals.success_message = req.flash('success_message')
+    res.locals.error_message = req.flash('error_message')
+    next()
+})
 app.listen(3000,()=>{
     db.sequelize.sync();
     console.log('hello');
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,12 +8,24 @@ const UserController = {
     },
     signUp(req,res){
         console.log(req.body)
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password,bcrypt.genSaltSync(10),null)
-        }).then(()=>{
-            return res.redirect('signin')
+        if(req.body.passwordCheck !== req.body.password){
+            req.flash('error_message','兩次密碼輸入不同！');
+            return res.redirect('signup');
+        }
+        User.findOne({where:{email:req.body.email}}).then(user=>{
+            if(user){
+                req.flash('error_message','信箱重複！');
+                return res.redirect('signup');
+            }else{
+                User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: bcrypt.hashSync(req.body.password,bcrypt.genSaltSync(10),null)
+                }).then(()=>{
+                    req.flash('success_message','成功註冊帳號！');
+                    return res.redirect('signin')
+                })
+            }
         })
     }
 }
