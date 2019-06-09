b1bdef9261ee1e93d59b2638f4fcac5776bca82b
leowang <rfv7855659@gmail.com>
2019-06-10 01:54:15 +0800

    make it better

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-var express = require('express');
-var handlebars = require('express-handlebars');
-var bodyParser = require('body-parser');
-var db = require('./models');
+const express = require('express')
+const handlebars = require('express-handlebars')
+const db = require('./models')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
-const passport = require('./config/passport');
-const methodOverride = require('method-override');
-// var port = process.env.PORT || 3000;
-var port = 3000;
-var app = express();
+const passport = require('./config/passport')
 
-app.engine('handlebars',handlebars({defaultLayout: 'main'}));
-app.set('view engine','handlebars')
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(flash());
+const app = express()
+const port = process.env.PORT || 3000
+
+// 設定 view engine 使用 handlebars
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride("_method"));
-app.use('/upload', express.static(__dirname + '/upload'))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+// app.use('/upload', express.static(__dirname + '/upload'))
 
-app.use((req,res,next)=>{
-    res.locals.success_message = req.flash('success_message')
-    res.locals.error_message = req.flash('error_message')
-    res.locals.user = req.user
-    next()
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
 })
 
-app.listen(port,()=>{
-    db.sequelize.sync();
-    console.log('hello');
+app.listen(port, () => {
+  db.sequelize.sync() // 跟資料庫同步
+  console.log(`Example app listening on port ${port}`)
 })
 
-require('./routes')(app,passport);
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app, passport)
