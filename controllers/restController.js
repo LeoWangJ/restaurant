352931b56cdd76c80d9fc9b6389f4818c8d869a9
leowang <rfv7855659@gmail.com>
2019-06-15 22:28:01 +0800

    完成前台餐廳總表

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,16 @@
+let db = require('../models');
+let Restaurant = db.Restaurant;
+let Category = db.Category;
+
 let restController = {
     getRestaurants: (req, res) =>{
-        return res.render('restaurants')
+        Restaurant.findAll({include:Category}).then(restaurant=>{
+            const data = restaurant.map(r=>({
+                ...r.dataValues,
+                description: r.dataValues.description.substring(0,50)
+            }))
+            return res.render('restaurants',{restaurants: data})
+        })
     }
 }
 
