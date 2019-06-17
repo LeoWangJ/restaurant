02963f34b5dfd65c8284afe063b1ab2d7cf156af
leowang <rfv7855659@gmail.com>
2019-06-18 00:23:37 +0800

    完成個人編輯功能以及顯示已評論過的餐廳

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcrypt-nodejs');
 const db = require('../models');
 const User = db.User;
+let Restaurant = db.Restaurant;
+let Comment = db.Comment;
+const imgur = require('imgur-node-api');
+const IMGUR_CLIENT_ID = "9e13906c7f09bc5";
 
 const UserController = {
     signUpPage(req, res) {
@@ -39,6 +43,49 @@ const UserController = {
         req.flash('success_messages','登出成功');
         req.logout();
         return res.redirect('/signin')
+    },
+    getUser(req,res){
+        User.findByPk(req.params.id).then(user=>{
+            Comment.findAll({include:Restaurant,where:{ UserId:user.id}}).then(comments=>{
+                return res.render('users',{user:user,comments:comments})
+            })
+        })
+    },
+    editUser(req,res){
+        User.findByPk(req.params.id).then(user=>{
+            return res.render(`editUser`,{user:user})
+        })
+    },
+    putUser(req,res){
+        var {file} = req;
+        if(file){
+            imgur.setClientID(IMGUR_CLIENT_ID);
+            imgur.upload(file.path,(err,img)=>{
+                return User.findByPk(req.params.id).then(user=>{
+                    if(req.params.id == user.id){
+                        user.update({
+                            name: req.body.name,
+                            image: file ? img.data.link : user.image
+                        }).then(user=>{
+                            req.flash('success_messages', 'user was successfully to update')
+                            res.redirect(`/users/${user.id}`)
+                        })
+                    }
+                })
+            })
+            
+        }else{
+            User.findByPk(req.params.id).then(user=>{
+                user.update({
+                    name: req.body.name,
+                    image: user.image
+                }).then(user=>{
+                    req.flash('success_messages', 'user was successfully to update')
+                    return res.redirect(`/users/${user.id}`)
+                })
+            })
+        }
+        
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -50,5 +50,7 @@ module.exports = (app,passport) => {
     app.get('/signin',userController.signInPage);
     app.post('/signin',passport.authenticate('local',{failureRedirect: '/signin', failureFlash: true}),userController.signIn);
     app.get('/logout',userController.logout);
-    
+    app.get('/users/:id',userController.getUser);
+    app.get('/users/:id/edit',userController.editUser);
+    app.put('/users/:id',upload.single('image'),userController.putUser);
 } 
