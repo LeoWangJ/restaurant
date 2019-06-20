583cd90b5d9b2e99ea55e3a488bd267e63f2bb25
leowang <rfv7855659@gmail.com>
2019-06-21 01:44:08 +0800

    新增followShip model

--- models/user.js
+++ models/user.js
@@ -19,6 +19,16 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey:'UserId',
       as: 'LikedRestaurants'
     })
+    User.belongsToMany(User,{
+      through: models.Followship,
+      foreignKey: 'FollowerId',
+      as: 'Followings'
+    })
+    User.belongsToMany(User,{
+      through: models.Followship,
+      foreignKey: 'FollowingId',
+      as: 'Followers'
+    })
   }
   return User
 }
